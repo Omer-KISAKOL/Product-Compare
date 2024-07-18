adc60d2541b195331ebaf3284a040e82c2e865f2
omer- <okisakol@gmail.com>
2024-07-18 17:12:14 +0300

    Compare complated

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,13 @@ import "./style.css"
 const product = ({product, compare}) => (
     <div key={product.id} className="col-sm-6 col-md-3">
         <div className={"product " + (product.compare ? "compare" : "")}>
+
             <img src={product.image} alt={product.name} />
             <div className="image_overlay"/>
             <div className="view_details" onClick={() => compare(product.id)}>
                 {product.compare ? "Remove" : "Compare"}
             </div>
+
             <div className="stats">
                 <div className="stats-container">
                     <span className="product_price">{product.price}</span>
@@ -15,6 +17,7 @@ const product = ({product, compare}) => (
                     <p>{product.description}</p>
                 </div>
             </div>
+
         </div>
     </div>
 )
